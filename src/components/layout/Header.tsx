a19ae2709d8e536b3b4b9e2f780265adbef39cb7
Anooj Singh <anujsingh49244@gmail.com>
2023-04-27 22:44:15 +0530

    Create responsive header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,15 +5,50 @@ import CartBag from "../../assests/svgs/CartBag";
 import { layout } from "../../styles/style";
 
 const Header = () => {
+  const menuItems = [
+    {
+      name: "Home",
+    },
+    {
+      name: "Shop",
+    },
+    {
+      name: "Featured",
+    },
+    {
+      name: "Layouts",
+    },
+    {
+      name: "Page",
+    },
+  ];
   return (
     <header className="h-[50px] bg-darkbg">
-      <div className={`${layout.flex.row.spaceBetween} h-full px-5`}>
-        <div>
-          <GiHamburgerMenu className="text-white text-3xl" />
+      <div
+        className={`${layout.flex.row.spaceBetween} h-full px-5 lg:px-[10%] w-full`}
+      >
+        <div className={`${layout.flex.row.center} h-full`}>
+          <ul
+            className={`hidden lg:flex ${layout.flex.row.left} text-white h-full cursor-pointer`}
+          >
+            {menuItems.map((eachItem) => (
+              <li className={`${layout.flex.row.center}hover:bg-red h-full px-5 mx-2`}>
+                {eachItem.name}
+              </li>
+            ))}
+          </ul>
+          {/* Menu icon */}
+          <GiHamburgerMenu className="text-white text-3xl lg:hidden" />
         </div>
-        <div className={`${layout.flex.row.left} gap-2`}>
-          <RxDividerVertical className="text-red text-4xl" />
-          <FaSearch className="text-white text-lg" />
+        <div className={`${layout.flex.row.left} gap-5`}>
+          {/* Menu icon */}
+          <GiHamburgerMenu className="text-white text-3xl hidden lg:block" />
+
+          {/* Search */}
+          <RxDividerVertical className="text-red text-4xl lg:hidden" />
+          <FaSearch className="text-white text-lg lg:hidden" />
+
+          {/* Cart Bag */}
           <CartBag />
         </div>
       </div>
